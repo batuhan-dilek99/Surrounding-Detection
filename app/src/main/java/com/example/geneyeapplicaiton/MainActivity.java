8ca14c5c3b7a00caee08882ad493045ccc3c9b97
batuhan-dilek99 <batuhandlek@gmail.com>
2023-01-22 14:37:40 +0300

    Comments

--- app/src/main/java/com/example/geneyeapplicaiton/MainActivity.java
+++ app/src/main/java/com/example/geneyeapplicaiton/MainActivity.java
@@ -21,28 +21,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Referancing to UI elements-----------------
         Btn = (Button)findViewById(R.id.cmr);
         Et1 = (EditText)findViewById((R.id.Et1));
         Et2 = (EditText)findViewById((R.id.Et2));
         tv = (TextView)findViewById((R.id.tv));
+        //-------------------------------------------
+
+        //Start python---------------------------------------------
         if(!Python.isStarted()){
             Python.start(new AndroidPlatform(this));
         }
 
         Python py = Python.getInstance();
         PyObject pyobj = py.getModule("deneme");  //Name of the python script
+        //---------------------------------------------------------
 
-
-
+        //Overriding button onClick event--------------------------
         Btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                PyObject obj = pyobj.callAttr("denemesum", Et1.getText().toString(), Et2.getText().toString());
+                PyObject obj = pyobj.callAttr("denemesum", Et1.getText().toString(), Et2.getText().toString());  //Function name from the script, function arg1, function arg2
 
                 tv.setText(obj.toString());
             }
         });
-
+        //---------------------------------------------------------
     }
 }
