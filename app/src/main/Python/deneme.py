c5f8b3214ce423a1dfbb68cdce5131fdff5a3b2f
batuhan-dilek99 <batuhandlek@gmail.com>
2023-01-22 14:34:17 +0300

    python integration

---
+++ app/src/main/Python/deneme.py
@@ -0,0 +1,7 @@
+def denemesum(num1,num2):
+    n1 = int(num1)
+    n2 = int(num2)
+
+    summ = n1 + n2
+
+    return "sum is " + str(summ)
--- app/src/main/java/com/example/geneyeapplicaiton/MainActivity.java
+++ app/src/main/java/com/example/geneyeapplicaiton/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.geneyeapplicaiton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText Et1, Et2;
+    Button Btn;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Btn = (Button)findViewById(R.id.cmr);
+        Et1 = (EditText)findViewById((R.id.Et1));
+        Et2 = (EditText)findViewById((R.id.Et2));
+        tv = (TextView)findViewById((R.id.tv));
+        if(!Python.isStarted()){
+            Python.start(new AndroidPlatform(this));
+        }
+
+        Python py = Python.getInstance();
+        PyObject pyobj = py.getModule("deneme");  //Name of the python script
+
+
+
+        Btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                PyObject obj = pyobj.callAttr("denemesum", Et1.getText().toString(), Et2.getText().toString());
+
+                tv.setText(obj.toString());
+            }
+        });
+
     }
 }
